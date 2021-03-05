db1abe2f14b85c645eeba325f92b4a313215497e
dep153 <dep153@msstate.edu>
2021-03-04 20:21:48 -0600

    Fixed various syntax errors

--- App.py
+++ App.py
@@ -3,39 +3,41 @@
 from BMI_Calculator import BMI_Calc
 from BMI_Calculator import Get_Category
 
-print("Which application would you like to run?")
-print("1) BMI Calculator")
-print("2) Retirement Tracker\n")
-
 while True:
 
-    ans = input("1 or 2: ")
+    print("Which application would you like to run?")
+    print("1) BMI Calculator")
+    print("2) Retirement Tracker\n")
+
+    ans = int(input("1 or 2: "))
 
     if ans == 1:
 
         print("Please input your height in feet and inches.\n")
 
-        feet = input("Feet: ")
+        feet = int(input("Feet: "))
         if feet == "x":
             break
 
-        inches = input("Inches: ")
+        inches = int(input("Inches: "))
         if inches == "x":
             break
 
         print("Please input your weight in pounds.\n")
 
-        pounds = input("Pounds: ")
+        pounds = int(input("Pounds: "))
         if pounds == "x":
             break
 
         BMI = BMI_Calc(feet, inches, pounds)
 
-        print("Your BMI is " + BMI + ".")
+        print("Your BMI is " + str(BMI) + ".")
 
         category = Get_Category(BMI)
+
+        print("You are " + category + ".\n")
         
-        print("You are " + category + ".")
+        input("Enter any value to continue: ")
 
     #elif ans == 2:
     #    Retire_Tracker()
